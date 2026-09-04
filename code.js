5e1515138f1cc41b541078e1ff6ded669feb8507
Alydiira <alydiiraprice@gmail.com>
2026-09-04 22:43:13 +1000

    functional skin color buttons

--- code.js
+++ code.js
@@ -1,37 +1,65 @@
 
 // Holds the states of each item
-var gameState = {
-    skin = 0,
+var state = {
+    skin : 0,
+    makeup : 0,
 };
 
 // Very simple state-swticher function -- make this more scalable
 function nextSkin() {
     console.log("inside function nextSkin");
-    var skin=document.getElementById("skin");
-    console.log("current state: ", state.skin);
+    console.log(state.skin);
+    var skinColor=document.getElementById("skin");
+    
 
-    if (gameState.skin===0) {
+    if (state.skin===0) {
         // change variable names eg olive-skin
-        skin.setAttribute("class", "skin1");
+        skinColor.setAttribute("class", "olive-skin");
         state.skin++;
         console.log(state.skin);
-    } else if (gameState.skin===1) {
+    } else if (state.skin===1) {
         // change variable names eg olive-skin
-        skin.setAttribute("class", "skin2");
+        skinColor.setAttribute("class", "purple-skin");
         state.skin++;
         console.log(state.skin);
-    } else if (gameState.skin===2) {
+    } else if (state.skin===2) {
         // change variable names eg olive-skin
-        skin.setAttribute("class", "skin3");
+        skinColor.setAttribute("class", "light-skin");
         state.skin=0;
         console.log(state.skin);
     }
 }
 
-// default vals on load
-function init () {
-    console.log("window loaded");
-    skin=0;
+function nextMakeup() {
+    console.log("inside function nextMakeup");
+    console.log(state.makeup);
+    var makeupColor=document.getElementById("makeup");
+    
+
+    if (state.makeup===0) {
+        // change variable names eg olive-skin
+        makeupColor.setAttribute("class", "purple-makeup");
+        state.makeup++;
+        console.log(state.makeup);
+    } else if (state.makeup===1) {
+        // change variable names eg olive-skin
+        makeupColor.setAttribute("class", "fancy-makeup");
+        state.makeup++;
+        console.log(state.skin);
+    } else if (state.makeup===2) {
+        // change variable names eg olive-skin
+        makeupColor.setAttribute("class", "simple-makeup");
+        state.makeup=0;
+        console.log(state.makeup);
+    }
 }
 
+// default vals on load
 window.onload=init;
+function init () {
+    console.log("window loaded");
+    // state.skin=0;
+    document.getElementById("skin").className="light-skin";
+    document.getElementById("makeup").className="simple-makeup";
+}
+
