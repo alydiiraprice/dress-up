1b4e90de4b7bba20d3c77897e87f79197594a458
Alydiira <alydiiraprice@gmail.com>
2026-09-04 23:22:56 +1000

    blonde hair added, brown hair frame needs to remove black outline

--- code.js
+++ code.js
@@ -4,6 +4,7 @@ var state = {
     skin : 0,
     makeup : 0,
     eyes : 0,
+    hair : 0
 };
 
 // Very simple state-swticher function -- make this more scalable
@@ -74,6 +75,21 @@ function nextEyes() {
     }
 }
 
+function nextHair() {
+    console.log("inside function nextHair");
+    var hairColor=document.getElementById("hair");
+    
+    if (state.hair===0) {
+        // change variable names eg olive-skin
+        hairColor.setAttribute("class", "brown-hair");
+        state.hair++;
+    } else if (state.hair===1) {
+        // change variable names eg olive-skin
+        hairColor.setAttribute("class", "blonde-hair");
+        state.hair = 0;
+    }
+}
+
 // default vals on load
 window.onload=init;
 function init () {
@@ -82,5 +98,6 @@ function init () {
     document.getElementById("skin").className="light-skin";
     document.getElementById("makeup").className="simple-makeup";
     document.getElementById("eyes").className="black-eyes";
+    document.getElementById("hair").className="blonde-hair";
 }
 
